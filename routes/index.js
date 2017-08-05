77505a4ad83e33674f0bad49ea7afef5b057b361
Samar Meena <23554168+samarmeena@users.noreply.github.com>
2017-08-05 10:15:52 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Expreses' });
+  res.render('index', { title: 'SamarMeena Express-Nodemailer' });
 });
 
 module.exports = router;
